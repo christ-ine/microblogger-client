3670a141f41bc887eceb7e40227467b1fdfa73e2
Christine Ross <christinejoyross@gmail.com>
2021-01-25 15:42:21 -0800

    add proxy and edit server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -5,7 +5,7 @@ import { ApolloProvider } from '@apollo/react-hooks'
 import { setContext } from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://microblogger49.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
